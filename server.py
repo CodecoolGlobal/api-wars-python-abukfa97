e72eca9c0a9a6a85e0230e67a9889bb7fd03e77f
Adrián Bükfa <bukfa.adrian@gmail.com>
2022-06-18 19:50:24 +0200

    Implement the index page with first 10 datas

--- server.py
+++ server.py
@@ -3,6 +3,11 @@ import requests, json
 
 app = Flask(__name__)
 
+@app.route('/')
+def route_index():
+    return render_template('index.html')
+
+
 if __name__ == '__main__':
     app.run(host='localhost',
             debug=True,
--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,40 @@
-// TODO : !!!! PLANETS !!!!
+let actual = 'https://swapi.dev/api/planets/'
+let planets;
+let next;
+let previous;
+
+function getPlanets(url) {
+    fetch(url).then((res) => res.json()).then((data) => {
+        next = data.next != null ? data.next : actual;
+        previous = data.previous === null ? data.previous : actual;
+        planets = data.results;
+        return createPlanetBoard(planets);
+    });
+}
+
+function createPlanetBoard(planets) {
+    for (let planet of planets) {
+        let planetBoard = document.querySelector('#planetBoard');
+        let row = document.createElement("tr")
+        row.innerHTML = `
+        <td id="name">${planet.name}</td>
+        <td id="diameter">${parseInt(planet.diameter).toLocaleString('en-Us')} km</td>
+        <td id="climate">${planet.climate}</td>
+        <td id="terrain">${planet.terrain}</td>`
+        row.innerHTML += planet.surface_water != 'unknown' ? `<td id="surface_water">${planet.surface_water} %</td>` : `<td id="surface_water">${planet.surface_water}</td>`;
+        row.innerHTML += planet.population != 'unknown' ? `
+        <td id="population">${parseInt(planet.population).toLocaleString('en-US')} peoples</td>` :
+            `<td>${planet.population}</td>`;
+        row.innerHTML += planet.residents != 0 ? `
+        <td id="residents">
+            <button id="${planet.name}" class="btn btn-secondary" type="button">${planet.residents.length} Resident(s)</button>
+        </td>
+        ` : `
+        <td id="residents">No known residents.</td>`;
+
+        planetBoard.appendChild(row);
+    }
+}
+
+getPlanets(actual);
+
