10e47b80323e1a77f99f63558eea78bec9926db1
Adrián Bükfa <bukfa.adrian@gmail.com>
2022-06-18 16:57:32 +0200

    Setup the project

--- server.py
+++ server.py
@@ -1,1 +1,9 @@
-# put your code here
+from flask import Flask, render_template,session,request,redirect
+import requests, json
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run(host='localhost',
+            debug=True,
+            port=5010)
